b77298dab619d924bd18fc26394e72359909823c
gentom <gento.m.developer@gmail.com>
2018-09-13 14:08:55 +0900

    init REPL

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"go/token"
+	"io"
+
+	"github.com/gentom/mkip/lexer"
+	"github.com/gentom/mkip/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
